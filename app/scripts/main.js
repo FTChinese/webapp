64da4fe96abd7e7cd40501b9ae637590eebf594f
wangyichen1064431086 <1064431086@qq.com>
2016-12-03 10:57:52 +0800

    close the swipe in android again

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -295,11 +295,11 @@ function startpage() {
     if (isOnline()=="possible") {checkbreakingnews();}
 
     autoPrefix();
-    /*
+    
     if(osVersion.indexOf("Android")<0){
          initSwipeGesture();
     }
-   */
+   
    initSwipeGesture();
 
     //Delegate Click Events for Any New Development
